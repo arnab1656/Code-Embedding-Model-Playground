9b7d5a7c4212a4a35fb64b6187981141ee68dddc
arnab1656 <arnab.paul.1656@gmail.com>
2025-09-20 20:47:33 +0530

    Runnable Implemented

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
 from Salesforce.sfr_embedding_code_400m_r_model import embedding_retreival
 from langchain_text_splitters import RecursiveCharacterTextSplitter
 from langchain_core.prompts import PromptTemplate
+from langchain_core.runnables import RunnableLambda
+from langchain_core.output_parsers import StrOutputParser
 from langchain_openai import OpenAI
 from dotenv import load_dotenv
 
@@ -9,8 +11,6 @@ load_dotenv()
 with open("py.txt","r") as f:
   code_text = f.read()
   
-# print(code_text)
-
 custom_separators = ['\nclass ','\ndef ', '\n\tdef ',]
 
 python_splitter = RecursiveCharacterTextSplitter(
@@ -23,23 +23,24 @@ python_splitter = RecursiveCharacterTextSplitter(
 
 python_docs = python_splitter.create_documents([code_text])
 
-query = "File handling in python"
+query = "how the file is handling is done in the code"
 
 top_k_retreival = embedding_retreival([query], python_docs)
 
-for i, top in enumerate(top_k_retreival):
-  print("-"*150)
-  print("result ->",top[i+1])
-  print("-"*150)
+# for i, top in enumerate(top_k_retreival):
+#   print("-"*150)
+#   print("result ->",top[i+1])
+#   print("-"*150)
 
-  
+parser = StrOutputParser()
+
 prompt = PromptTemplate.from_template(
 """
 You are an AI coding assistant. 
-The user will provide a code reference or snippet, and you need to:
-1. Explain what the code does in simple terms.
-2. Suggest improvements or best practices if applicable.
-3. Provide example usage if relevant.
+A code reference or snippet is provided, and you need to:
+1. Answer the Query asked to you.
+2. If Answer is not known, Do not answer it or hallucinate it. "Say I dont know"
+3. Strictly follow the Context of Code.
 
 Code Snippet:
 {code_reference}
@@ -50,11 +51,11 @@ query:
 """
 )
 
-code_docs = " ".join( top[i+1] for i,top in enumerate(top_k_retreival))
+retreival_runnable = RunnableLambda(lambda args : { "code_reference" : " ".join(top[i+1] for i,top in enumerate(embedding_retreival(docs = args["python_docs"], query = args["query"]))), "query" : query })
 
 llm = OpenAI()
-chain = prompt | llm
+chain  = retreival_runnable | prompt | llm | parser
 
-res = chain.invoke({"code_reference": code_docs, "query" : query})
+res = chain.invoke({"python_docs": python_docs, "query" : query})
 
 print("res -> \n",res)
